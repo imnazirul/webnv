7736ac85c4630246ef91f2198bbfe766bd3d7bba
MD Nazirul Islam <139609914+webnpage@users.noreply.github.com>
2023-11-23 00:26:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function openMenu() {
+function openMenu(_this) {
     const side = document.querySelector('.side-bar')
 
     side.classList.toggle('show')
+  if(side.classList.contains('show')){
+    _this.innerHTML ="&#10005"
+  }else{
+    _this.innerHTML = ""
+  }
+
 }
 
 function goHtml(url) {
