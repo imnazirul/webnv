fe2ea570fef98ba9c7e176c0f8c43a7ceb4d027a
MD Nazirul Islam <139609914+webnpage@users.noreply.github.com>
2023-11-23 00:30:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function openMenu(_this) {
     const side = document.querySelector('.side-bar')
+    const thisEl = _this;
 
     side.classList.toggle('show')
-  if(side.classList.contains('show')){
-    _this.innerHTML ="&#10005"
-  }else{
-    _this.innerHTML = ""
+
+    if(side.classList.contains('show')){
+       thisEl.innerHTML ="&#10005"
+    }else{
+       thisEl.innerHTML = '<i class="fa-solid fa-bars"></i>'
   }
 
 }
